e59b2d2db6715b6441fe65fca10cf42987f945be
rad8n <74486357+rad8n@users.noreply.github.com>
2026-08-03 16:48:33 -0700

    Add components export entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/components.ts"
-  ],
+  entry: {
+    index: "src/index.ts",
+    components: "src/components.ts",
+  },
   format: ["esm"],
   dts: true,
-  clean: true,
   sourcemap: true,
+  clean: true,
 })
