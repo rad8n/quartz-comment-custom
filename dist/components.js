bdbcbbaeee72820251cc9cc402f438313540d16b
rad8n <74486357+rad8n@users.noreply.github.com>
2026-08-03 17:06:22 -0700

    simplify comments component render

--- dist/components.js
+++ dist/components.js
@@ -1,11 +1,12 @@
 // src/comments.tsx
 import { jsx, jsxs } from "preact/jsx-runtime";
-var Comments = ({ fileData }) => {
+var Comments = (props) => {
+  const fileData = props?.fileData;
   return /* @__PURE__ */ jsxs("section", { className: "comments", children: [
     /* @__PURE__ */ jsx("h3", { children: "Comments" }),
     /* @__PURE__ */ jsxs("p", { children: [
-      "Comments loaded for ",
-      fileData.slug
+      "Comments loaded",
+      fileData?.slug ? ` for ${fileData.slug}` : ""
     ] })
   ] });
 };
--- src/comments.tsx
+++ src/comments.tsx
@@ -1,11 +1,14 @@
 import type { QuartzComponent } from "@quartz-community/types"
 
-export const Comments: QuartzComponent = ({ fileData }) => {
+export const Comments: QuartzComponent = (props) => {
+  const fileData = props?.fileData
+
   return (
     <section className="comments">
       <h3>Comments</h3>
       <p>
-        Comments loaded for {fileData.slug}
+        Comments loaded
+        {fileData?.slug ? ` for ${fileData.slug}` : ""}
       </p>
     </section>
   )
